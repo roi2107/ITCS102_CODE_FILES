6735bcddde07a6664642845bd91861a3db95b793
roi2107 <johnroitesiorna11@gmail.com>
2026-08-17 11:28:37 +0800

    Python activity 3

---
+++ Activity3.py
@@ -0,0 +1,46 @@
+#int
+age = 18
+shoe_size = 40
+height = 170
+money_on_wallet = 100
+
+print("my age is", age)
+print("my shoe size is", shoe_size)
+print("my height is", height)
+print("my money rn is", money_on_wallet)
+
+#float
+change = 27.30
+bills_water = 230.14
+bills_electricity = 1000.50
+total_salary = 26034.32
+
+print("my change from 7 eleven is", change)
+print("our bills from primewater is", bills_water)
+print("our electricity bill from meralco is", bills_electricity)
+print("my father salary is ", total_salary)
+
+#string
+crush = "alfred"
+father = "alfred"
+mother = "alfred"
+grandgrandgrandmother = "alfred"
+
+print("my crush is", crush)
+print("my father is", father)
+print("my mother is", mother)
+print("my lolalolalolalola is", grandgrandgrandmother)
+
+#boolean
+rich = False
+poor = True
+gay = True
+straight = False
+
+print("alfred is rich", rich)
+print("alfred is poor", poor)
+print("alfred is gay", gay)
+print("alfred is not gay", straight)
+
+
+ 
